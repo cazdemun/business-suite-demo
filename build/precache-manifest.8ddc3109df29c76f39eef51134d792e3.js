701fdc2f85e45883397b94428260037f79f12a08
cazdemun <cplu1509@gmail.com>
2020-08-17 11:43:33 -0500

    First deploy

---
+++ build/precache-manifest.8ddc3109df29c76f39eef51134d792e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d675b5172ec0b379235dad02761ceeaa",
+    "url": "/business-utilities-suite-demo/index.html"
+  },
+  {
+    "revision": "30073fc7688129d43dcb",
+    "url": "/business-utilities-suite-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34452783fe0af63ecbf4",
+    "url": "/business-utilities-suite-demo/static/js/2.ae660645.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/business-utilities-suite-demo/static/js/2.ae660645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30073fc7688129d43dcb",
+    "url": "/business-utilities-suite-demo/static/js/main.3e754983.chunk.js"
+  },
+  {
+    "revision": "24e23d9dc857107085fb",
+    "url": "/business-utilities-suite-demo/static/js/runtime-main.50db7367.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/business-utilities-suite-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
